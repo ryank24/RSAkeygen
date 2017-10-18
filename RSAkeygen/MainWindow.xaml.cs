9dfb57dceded1dd3dd3c989fc1c74103e9db4111
Kyle Tillotson <kylejtillotson@gmail.com>
2017-10-18 13:07:35 -0500

    meh
    foo

--- RSAkeygen/MainWindow.xaml.cs
+++ RSAkeygen/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Security.Cryptography;
+using System.IO;
 
 namespace RSAkeygen
 {
@@ -20,6 +22,9 @@ namespace RSAkeygen
     /// </summary>
     public partial class MainWindow : Window
     {
+        int keysize = 0;
+        static String path = @"C:\temp";
+        DirectoryInfo di = Directory.CreateDirectory(path);
         public MainWindow()
         {
             InitializeComponent();
@@ -27,17 +32,52 @@ namespace RSAkeygen
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            tblock1.Text = "You generated an RSA key pair";
+            if(Int32.TryParse(KeySizeField.Text, out keysize))
+            {
+                if((keysize >= 512) && (keysize <= 16384) && ((keysize & (keysize - 1)) == 0)) {
+                    try
+                    {
+                        RSACryptoServiceProvider RSAalg = new RSACryptoServiceProvider(keysize);
+                        byte[] keyinfo = RSAalg.ExportCspBlob(true);
+                        File.WriteAllBytes(@"C:\temp\foo.txt", keyinfo);
+                    }
+                    catch(ArgumentNullException)
+                    {
+                        Console.WriteLine("Key generation failed");
+                    }
+                    tblock1.Text = "You generated an RSA key pair of size " + KeySizeField.Text;
+                }
+                else
+                {
+                    tblock1.Text = "That is not a valid keysize";
+                }
+            }
+            else
+            {
+                tblock1.Text = "That is not a valid keysize";
+            }
+            
         }
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
-
+            openInExplorer(@"C:\temp");
+            byte[] keyinfo = File.ReadAllBytes(@"C:\temp\foo.txt");
+            RSACryptoServiceProvider RSAalg = new RSACryptoServiceProvider();
+            RSAalg.ImportCspBlob(keyinfo);
+            loadedstatusblock.Text = "Keysize " + RSAalg.KeySize + " loaded";
         }
 
         private void Button_Click_2(object sender, RoutedEventArgs e)
         {
 
+        }
+
+        static void openInExplorer(string path)
+        {
+            string cmd = "explorer.exe";
+            string arg = "/select, " + path;
+            System.Diagnostics.Process.Start(cmd, arg);
         }
     }
 }
